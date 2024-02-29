5d254cd43193a108f1b10f72a6e91c56f7d94786
Roberto <rob.riveros.es@gmail.com>
2024-02-29 20:01:35 +0100

    create second exercise - digital clock

--- src/app/digital-clock/page.jsx
+++ src/app/digital-clock/page.jsx
@@ -1,7 +1,10 @@
+import Digitalclock from "@/components/DigitalClock";
+
 const page = () => {
   return (
-    <div className="flex flex-col gap-5 justify-center">
-      <h2>Ejercicio 2</h2>
+    <div className="ml-72 pt-10 flex flex-col gap-5 justify-center">
+      <h2 className="text-2xl">Digital Clock</h2>
+      <Digitalclock />
     </div>
   );
 };
